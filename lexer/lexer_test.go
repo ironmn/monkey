338fdb7a73c10bf5f0af03a82f52056de49f8b08
lelouch <3298613475@qq.com>
2023-01-05 20:12:07 +0800

    添加了新的扩展测试用例，用于测试NextToken的完整性

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -35,3 +35,70 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 }
+
+/**
+是对于上面测试用例的扩展，基本覆盖了Monkey语言所有可能出现的内容
+*/
+func TestExtendedNextToken(t *testing.T) {
+	input := `let six = 6;
+let ten = 10;
+let add = fn(x, y){
+	x + y;
+};
+
+let result = add(six, ten);
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "six"},
+		{token.ASSIGN, "="},
+		{token.INT, "6"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "six"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong, expected=%q, actual=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong, expected=%q, actual=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
